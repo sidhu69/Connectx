b999660bd6a7aa5cedc44aa87f1fadb8dce3598a
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2026-02-05 17:43:33 +0530

    Create gurbani_data.dart

---
+++ lib/gurbani/gurbani_data.dart
@@ -0,0 +1,13 @@
+import 'bani_japji_sahib.dart';
+import 'bani_jaap_sahib.dart';
+import 'bani_anand_sahib.dart';
+// Add more bani imports here as you create them
+
+/// Central Gurbani registry
+/// Key  -> Gurbani title
+/// Value -> Full Gurbani text
+final Map<String, String> gurbaniText = {
+  japjiSahibTitle: japjiSahibText,
+  jaapSahibTitle: jaapSahibText,
+  anandSahibTitle: anandSahibText,
+};
